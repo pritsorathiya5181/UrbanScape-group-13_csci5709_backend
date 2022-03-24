2ec9a0c81eb88af9f4284a0680042f711ae1e29d
Prit Ajaykumar Sorathiya <pr669751@dal.ca>
2022-03-24 18:07:22 -0300

    Add service and order APIs

--- controllers/order.js
+++ controllers/order.js
@@ -1,13 +1,205 @@
 const Order = require('../models/orders/Orders')
+const nodemailer = require('nodemailer')
+
+exports.fetchOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.find()
+
+    if (orders.length > 0) {
+      req.orders = orders
+      next()
+    } else {
+      const errorResponse = {
+        message: 'No services found',
+        success: false,
+      }
+      res.status(404).json(errorResponse)
+    }
+  } catch (err) {
+    const errorResponse = {
+      message: err,
+      success: false,
+    }
+    res.status(500).json(errorResponse)
+  }
+}
 
 exports.getAllOrders = async (req, res, next) => {
-    const query = req.query.new
-    try {
-      const orders = query
-        ? await Order.find().sort({ _id: -1 }).limit(5)
-        : await Order.find()
-      res.status(200).json(orders)
-    } catch (err) {
-      res.status(500).json(err)
+  const query = req.query.new
+  try {
+    const orders = query
+      ? await Order.find().sort({ _id: -1 }).limit(5)
+      : await Order.find()
+
+    if (orders.length > 0) {
+      const successResponse = {
+        message: 'Orders fetched successfully',
+        success: true,
+        orders: orders,
+      }
+      res.status(200).json(successResponse)
+    } else {
+      const errorResponse = {
+        message: 'No Orders found',
+        success: false,
+      }
+      res.status(404).json(errorResponse)
     }
+  } catch (err) {
+    const errorResponse = {
+      message: err,
+      success: false,
+    }
+    res.status(500).json(errorResponse)
   }
+}
+
+exports.approveServiceRequest = async (req, res, next) => {
+  const { orderId } = req.params
+  const {
+    itemNo,
+    clientName,
+    clientEmail,
+    serviceCategory,
+    serviceName,
+    professionalName,
+  } = req.body
+
+  let isChanged = false
+  try {
+    const order = await Order.findOne({ orderId: orderId })
+    if (order) {
+      order.orderDetails = order.orderDetails.map((order, index) => {
+        if (order.itemNo.toString() === itemNo.toString()) {
+          {
+            isChanged = true
+            var orderResponse = JSON.parse(JSON.stringify(order))
+            orderResponse.orderItemStatus = 'Approved'
+            orderResponse.professionalName = professionalName
+            return orderResponse
+          }
+        }
+        return order
+      })
+
+      if (isChanged) {
+        await order.save()
+
+        var transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: process.env.email,
+            pass: process.env.pass,
+          },
+        })
+
+        var mailOptions = {
+          from: process.env.email,
+          to: clientEmail,
+          subject: 'Sending Email using Node.js',
+          html: `<!DOCTYPE html>
+          <html>
+          <body>
+          
+          <h2>UrbanScape</h2>
+          <p>Thank you for booking our service</p>
+          <p>Hey! ${clientName}, your request has been accepted, and our professional will visit you at home at the time you specified.</p>
+          <p>Service and Professional Details:</p>
+          <ul>
+            <li>Professional: <i>${professionalName}</i></li>
+              <li>Service Category: <i>${serviceCategory}</i></li>
+              <li>Service Name: <i>${serviceName}</i></li>
+          </ul>
+          
+          </body>
+          </html>
+          `,
+        }
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log('Email sending error: ', error)
+            const errorResponse = {
+              message: 'No Service found',
+              success: false,
+            }
+            res.status(404).json(errorResponse)
+          } else {
+            const successResponse = {
+              message: 'Service Approved! Email sent to client',
+              success: true,
+            }
+            res.status(200).json(successResponse)
+          }
+        })
+      } else {
+        const errorResponse = {
+          message: 'No Service found',
+          success: false,
+        }
+        res.status(404).json(errorResponse)
+      }
+    } else {
+      const errorResponse = {
+        message: 'No Service found',
+        success: false,
+      }
+      res.status(404).json(errorResponse)
+    }
+  } catch (err) {
+    const errorResponse = {
+      message: err,
+      success: false,
+    }
+    res.status(500).json(errorResponse)
+  }
+}
+
+exports.cancelServiceRequest = async (req, res, next) => {
+  const { orderId, itemNo } = req.params
+  let isChanged = false
+  try {
+    const order = await Order.findOne({ orderId: orderId })
+    if (order) {
+      order.orderDetails = order.orderDetails.map((order, index) => {
+        if (order.itemNo.toString() === itemNo) {
+          {
+            isChanged = true
+            var orderResponse = JSON.parse(JSON.stringify(order))
+            orderResponse.orderItemStatus = 'Cancelled'
+            return orderResponse
+          }
+        }
+        return order
+      })
+
+      if (isChanged) {
+        await order.save()
+        const successResponse = {
+          message: 'Service Approved',
+          success: true,
+        }
+
+        res.status(200).json(successResponse)
+      } else {
+        const errorResponse = {
+          message: 'No Service found',
+          success: false,
+        }
+        res.status(404).json(errorResponse)
+      }
+    } else {
+      const errorResponse = {
+        message: 'No Service found',
+        success: false,
+      }
+      res.status(404).json(errorResponse)
+    }
+  } catch (err) {
+    const errorResponse = {
+      message: err,
+      success: false,
+    }
+    res.status(500).json(errorResponse)
+  }
+}
--- routes/professional/service.js
+++ routes/professional/service.js
@@ -1,11 +1,23 @@
 const router = require('express').Router()
 const serviceController = require('../../controllers/professional/service')
+const orderController = require('../../controllers/order')
+
+//service stats
+router.get(
+  '/stats/:professionalId',
+  orderController.fetchOrders,
+  serviceController.fetchAllMyServices,
+  serviceController.getServiceStats
+)
 
 //add service
 router.post('/addService', serviceController.addService)
 
 //get all services
 router.get('/services', serviceController.getAllServices)
+
+//get all my services
+router.get('/services/:professionalId', serviceController.getAllMyServices)
 
 //get service
 router.get('/:serviceId', serviceController.getService)
