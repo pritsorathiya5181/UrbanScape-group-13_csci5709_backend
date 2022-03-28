5dcdbf4d3495fe015b043149dbbb645fc0acb8ea
Prit Ajaykumar Sorathiya <pr669751@dal.ca>
2022-03-28 17:48:02 -0300

    Update PORT

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ mongoose
   )
   .then((result) => {
     console.log('Connected to mongoDB successfully')
-    app.listen(5000)
+    app.listen(process.env.PORT || 5000)
   })
   .catch((err) => console.log(err))
 
