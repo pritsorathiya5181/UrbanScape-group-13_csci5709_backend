6785bc1ca5b41bfb2e46906e59f0f32e2f38e7f9
Priti Pandey <pr842219@dal.ca>
2022-04-06 12:37:44 -0300

    Adding email trigger for newsletter subscription

--- controllers/newslettersubscription.js
+++ controllers/newslettersubscription.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const NewsletterSubscription = require('../models/NewsletterSubscription')
+const nodemailer = require('nodemailer')
 const { v4: uuidv4 } = require('uuid')
 
 exports.addSubscription = async (req, res, next) => {
@@ -27,8 +28,50 @@ exports.addSubscription = async (req, res, next) => {
     });
     const conf = await newSubscription.save().then((result)=>{
         console.log(result);
+        if(result) {
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: process.env.email || 'fresky.france@gmail.com',
+                  pass: process.env.pass || 'mynameisprit@5181',
+                },
+              });
+              let mailOptions = {
+                from: process.env.email,
+                to: email,
+                subject: `Newsletter Subscription`,
+                html: `<!DOCTYPE html>
+                <html>
+                <body>
+                
+                <h2>UrbanScape</h2>
+                <p>Thank you for subscribing to our Newsletter !</p>
+                <p>Stay tuned for our latest exciting updates, offers and much more ! </p>
+                
+                </body>
+                </html>
+                `,
+              };
+              transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                  console.log('Email sending error: ', error)
+                  const errorResponse = {
+                    message: 'Email Error',
+                    success: false,
+                  }
+                  res.status(404).json(errorResponse)
+                } else {
+                  const successResponse = {
+                    message: 'Email Sent',
+                    success: true,
+                  }
+                  res.status(200).json(successResponse)
+                }
+              });
+              
+        }
         return res.status(200).json({ message: "New email subscription added", success: 'true' });
-      })
+      });
    
     
 } catch (err) {
